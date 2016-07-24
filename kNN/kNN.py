387b6c8ba3fc160aef16abcd6d6587c3bdbb1182
jwwangchn <jwwangchn@outlook.com>
2016-07-24 19:48:12 +0800

    在根目录添加main.py文件 创建kNN文件夹及文件 测试了第一个kNN分类程序

---
+++ kNN/kNN.py
@@ -0,0 +1,29 @@
+#coding:utf8
+from numpy import *
+import operator
+#训练数据
+def createDataSet():
+    group=array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+    labels=['A','A','B','B']
+    return group,labels
+
+#kNN算法
+def classify0(inX,dataSet,labels,k):
+    #inX为输入向量，dataSet为训练样本集，labels为训练样本标签
+    dataSetSize=dataSet.shape[0]  #训练样本集数据量大小
+    print '训练样本集大小:',dataSetSize
+    diffMat=tile(inX,(dataSetSize,1))-dataSet   #向量运算
+    sqDiffMat=diffMat**2     #矩阵的每一个元素进行平方运算
+    sqDistance=sqDiffMat.sum(axis=1)
+    distances=sqDistance**0.5
+    sortedDistIndicies=distances.argsort()  #返回排序后元素索引
+    classCount={}
+    for i in range(k):
+        voteIlabel=labels[sortedDistIndicies[i]]    #返回距离最近的几个元素的标签
+        classCount[voteIlabel]=classCount.get(voteIlabel,0)+1
+    sortedClassCount=sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse=True)
+    return sortedClassCount[0][0]
+
+group,labels=createDataSet()
+inXLabels=classify0([0,0],group,labels,3)
+print '输入数据的类别为:',inXLabels
