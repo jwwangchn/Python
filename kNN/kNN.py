f49497d3137b3dddde3ebf434bf1e0f6b6d052c7
jwwangchn <jwwangchn@outlook.com>
2016-07-24 21:24:57 +0800

    移动数据文件目录 完善kNN.py

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -1,4 +1,6 @@
 #coding:utf8
+import matplotlib
+import matplotlib.pyplot as plt
 from numpy import *
 import operator
 #训练数据
@@ -11,7 +13,6 @@ def createDataSet():
 def classify0(inX,dataSet,labels,k):
     #inX为输入向量，dataSet为训练样本集，labels为训练样本标签
     dataSetSize=dataSet.shape[0]  #训练样本集数据量大小
-    print '训练样本集大小:',dataSetSize
     diffMat=tile(inX,(dataSetSize,1))-dataSet   #向量运算
     sqDiffMat=diffMat**2     #矩阵的每一个元素进行平方运算
     sqDistance=sqDiffMat.sum(axis=1)
@@ -29,15 +30,56 @@ def file2matrix(filename):
     fr=open(filename)
     arrayOLines=fr.readlines()
     numberOfLines=len(arrayOLines)
-    returnMat=zeros((numberOfLines,3)
+    returnMat=zeros((numberOfLines,3))
     classLabelVector=[]
     index=0
     for line in arrayOLines:
         line=line.strip()
         listFromLine=line.split('\t')
         returnMat[index,:]=listFromLine[0:3]
-        classLabelVecor.append(int(listFromLine))
+        if listFromLine[-1] == 'largeDoses':
+            classLabelVector.append(1)            
+        elif listFromLine[-1] == 'smallDoses':
+            classLabelVector.append(2)
+        elif listFromLine[-1] == 'didntLike':
+            classLabelVector.append(3)
+        index+=1
+    return returnMat,classLabelVector
 
 group,labels=createDataSet()
 inXLabels=classify0([0,0],group,labels,3)
 print '输入数据的类别为:',inXLabels
+
+def autoNorm(dataSet):
+    minVals=dataSet.min(0)
+    maxVals=dataSet.max(0)
+    ranges=maxVals-minVals
+    normDataSet=zeros(shape(dataSet))
+    m=dataSet.shape[0]
+    normDataSet=dataSet-tile(minVals,(m,1))
+    normDataSet=normDataSet/tile(ranges,(m,1))
+    return normDataSet,ranges,minVals
+
+def datingClassTest():
+    hoRatio=0.1
+    datingDataMat,datingLabels=file2matrix('datingTestSet.txt')
+    normMat,ranges,minVals=autoNorm(datingDataMat)
+    m=normMat.shape[0]
+    numTestVecs=int(m*hoRatio)
+    errorCount=0.0
+    for i in range(numTestVecs):
+        classifierResult=classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+        print 'the classifier came back with: %d,the real answer is: %d' % (classifierResult,datingLabels[i])
+        if classifierResult != datingLabels[i]:
+            errorCount+=1.0
+    print 'the total arror rate is: %f' % (errorCount/float(numTestVecs))
+
+# #绘制散点图
+# fig1=plt.figure()
+# ax1=fig1.add_subplot(111)
+# ax1.scatter(datingDataMat[:,1],datingDataMat[:,2],15*array(datingLabels),15*array(datingLabels))
+# fig2=plt.figure()
+# ax2=fig2.add_subplot(111)
+# ax2.scatter(datingDataMat[:,0],datingDataMat[:,1],15*array(datingLabels),15*array(datingLabels))
+# plt.show()
+datingClassTest()
