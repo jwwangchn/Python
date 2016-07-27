e47615667603dec1d0df71f32a1743d2a4508640
jwwangchn <jwwangchn@outlook.com>
2016-07-27 18:34:44 +0800

    Updata decision tree

--- MachineLearinginAction/ch3/tree.py
+++ MachineLearinginAction/ch3/tree.py
@@ -1,9 +1,10 @@
 #coding:utf8
 import operator
+import treePlotter
 from math import log
 def createDataSet():
     dataSet=[[1,1,'yes'],
-            [1,1,'yes'],
+            [1,1,''],
             [1,0,'no'],
             [0,1,'no'],
             [0,1,'no']]
@@ -58,7 +59,7 @@ def majorityCnt(classList):
         if vote not in classCount.keys():
             classCount[vote]=0
             classCount[vote]+=1
-        sortedClassCount=sorted(classCount.itreitems(),key=operator.itemgetter(1),reverse=True)
+        sortedClassCount=sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse=True)
     return sortedClassCount[0][0]
 
 def createTree(dataSet,labels):
@@ -78,8 +79,32 @@ def createTree(dataSet,labels):
         myTree[bestFeatLabel][value]=createTree(splitDataSet(dataSet,bestFeat,value),subLabels)
     return myTree
 
+def classify(inputTree,featLabels,testVec):
+    firstStr=inputTree.keys()[0]
+    secondDict=inputTree[firstStr]
+    featIndex=featLabels.index(firstStr)
+    for key in secondDict.keys():
+        if testVec[featIndex] == key:
+            if type(secondDict[key]).__name__=='dict':
+                classLabel=classify(secondDict[key],featLabels,testVec)
+            else:
+                classLabel=secondDict[key]
+    return classLabel
+
+def storeTree(inputTree,filename):
+    import pickle
+    fw=open(filename,'w')
+    pickle.dump(inputTree,fw)
+    fw.close()
+
+def grabTree(filename):
+    import pickle
+    fr=open(filename)
+    return pickle.load(fr)
+
+
 myDat,labels=createDataSet()
-print myDat
+
 print calcShannonEnt(myDat)
 
 print splitDataSet(myDat,0,1)
@@ -87,3 +112,14 @@ print splitDataSet(myDat,0,0)
 print chooseBestFeatureToSplit(myDat)
 
 print createTree(myDat,labels)
+
+myTree=treePlotter.retrieveTree(0)
+myDat,labels=createDataSet()
+print labels
+print myTree
+print classify(myTree,labels,[1,0])
+print classify(myTree,labels,[1,1])
+
+storeTree(myTree,'classifierStorage.txt')
+print grabTree('classifierStorage.txt')
+
